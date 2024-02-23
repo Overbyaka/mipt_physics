305e97fdeacd92e41f0403a0a64039e4e51be5dd
Overbyaka <malenkayaelfiyka@yandex.ru>
2024-02-23 14:47:28 +0300

    hw 1 2d rework

--- lecture_1/harmonic_oscillator_2D.py
+++ lecture_1/harmonic_oscillator_2D.py
@@ -4,17 +4,21 @@ import math
 import sympy as sp
 # Проверка наших уравнений
 x = sp.Function('x')
-# l - длина от одной стены, до другой
-t,k_1, k_2, m, d, l = sp.symbols('t, k_1, k_2, m, d, l')
-eq = sp.Eq(-k_1*x(t) + k_2*(l-x(t)), m*x(t).diff(t,t))
+y = sp.Function('y')
+# l - длина от одной стены, до другой, h - высота между пружинами
+t,k_1, k_2, m, d, l, h = sp.symbols('t, k_1, k_2, m, d, l, h')
+eqx = sp.Eq(-k_1*x(t) + k_2*(l-x(t)), m*x(t).diff(t,t))
+eqy = sp.Eq(-k_1*y(t) + k_2*(h-y(t)), m*y(t).diff(t,t))
 
 # решает уравнение относительно t
 # с начальными условиями
-sol = sp.dsolve(eq, ics={x(0): 10, sp.diff(x(t), t).subs(t,0): 5, })
-sol_t = sp.lambdify(t, sol.rhs.subs({k_1: 3, k_2: 3, m:1, d:0.1, l: 20}))
+solx = sp.dsolve(eqx, ics={x(0): 10, sp.diff(x(t), t).subs(t,0): 7.5, })
+soly = sp.dsolve(eqy, ics={y(0): 1, sp.diff(y(t), t).subs(t,0): 1, })
+sol_tx = sp.lambdify(t, solx.rhs.subs({k_1: 3, k_2: 3, m:1, d:0.1, l: 20}))
+sol_ty= sp.lambdify(t, soly.rhs.subs({k_1: 3, k_2: 3, m:1, d:0.1, h: 2}))
 
 #%%
-sol_t
+sol_ty
 
 #%%
 import matplotlib
@@ -31,16 +35,17 @@ TOTAL_FRAMES = ANIM_TIME * FPS
 
 ts = np.linspace(0, ANIM_TIME, FPS * ANIM_TIME)
 
-anim_f = sol_t(ts)
+anim_fx = sol_tx(ts)
+anim_fy = sol_ty(ts)
 
 def animate(frame):
-    x = anim_f[frame]
+    x = anim_fx[frame]
+    y = anim_fy[frame]
     l = 20
     h = 2;
 
-    #y координату находим с помощью теоремы Пифагора
-    line_1.set_data(*spring((0, 0), (x, x*h/l), 25, 0.5))
-    line_2.set_data(*spring((l, h), (x, x*h/l), 25, 0.5))
+    line_1.set_data(*spring((0, 0), (x, y), 25, 0.5))
+    line_2.set_data(*spring((l, h), (x, y), 25, 0.5))
 
     return line_1, line_2
 
